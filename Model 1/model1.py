c1b635771409dfa44c369edb5a6546cfd566d4ed
Xin <33702741+chaidamu519@users.noreply.github.com>
2018-07-22 14:01:22 -0400

    Add files via upload

---
+++ Model 1/model1.py
@@ -0,0 +1,99 @@
+
+
+import pandas as pd
+import numpy as np
+
+samples = pd.read_csv('./driving_log.csv',  header=None)
+
+samples.columns = ['Center_image', 'Left_image', 'Right_image', 'steering angle', 'Throttle', 'Break', 'Speed']
+
+
+## drop Throttle, Break and Speed columns
+samples = samples.drop(['Throttle', 'Break', 'Speed'], axis = 1)
+
+## Correction factor for left and right images
+correction = 0.25
+sample_center = samples.drop(['Left_image', 'Right_image'], axis = 1).rename(index=str, columns={"Center_image": "img_path", "steering angle": "angles"})
+sample_left = samples.drop(['Center_image', 'Right_image'], axis = 1).rename(index=str, columns={"Left_image": "img_path", "steering angle": "angles"})
+sample_right = samples.drop(['Center_image', 'Left_image'], axis = 1).rename(index=str, columns={"Right_image": "img_path", "steering angle": "angles"})
+sample_left['angles'] += 0.2
+sample_right['angles'] -= 0.2
+data_set = pd.concat([sample_center, sample_left, sample_right], ignore_index=True)
+
+
+from sklearn.model_selection import train_test_split
+train_samples, validation_samples = train_test_split(data_set, test_size=0.2)
+
+
+from keras.models import Sequential
+from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Convolution2D, Dropout
+from sklearn.utils import shuffle
+import cv2
+from keras.models import Model
+import matplotlib.pyplot as plt
+
+def generator(samples, batch_size=32):
+    num_samples = len(samples)
+    while 1: # Loop forever so the generator never terminates
+        shuffle(samples)
+        for offset in range(0, num_samples, batch_size):
+            batch_samples = samples[offset:offset+batch_size]
+            images = []
+            angles = []
+            for ii in range(len(batch_samples) - 1):
+                name = './Datas/data/IMG/'+batch_samples.iloc[ii][0].split('/')[-1]
+                image = cv2.imread(name)
+                angle = float(batch_samples.iloc[ii][1])
+                images.append(image)
+                angles.append(angle)
+
+            # trim image to only see section with road
+            X_train = np.array(images)
+            y_train = np.array(angles)
+            yield (X_train, y_train)
+
+# compile and train the model using the generator function
+train_generator = generator(train_samples, batch_size=32)
+validation_generator = generator(validation_samples, batch_size=32)
+
+ch, row, col = 3, 160, 320  # Trimmed image format
+
+model = Sequential()
+# Preprocess incoming data, centered around zero with small standard deviation 
+model.add(Lambda(lambda x: x/127.5 - 1., input_shape=(row, col, ch), output_shape=(row, col, ch)))
+model.add(Cropping2D(cropping=((70,25), (0,0)), input_shape=(row, col, ch)))
+
+## Model Architecture (Nvidia)
+model.add(Convolution2D(24, 5, 5, subsample=(2, 2), input_shape=(3, 65, 320), activation='relu'))
+model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(48, 5, 5, subsample=(2, 2), activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(Flatten())
+model.add(Dropout(0.5))
+model.add(Dense(100, activation = 'relu'))
+model.add(Dropout(0.25))
+model.add(Dense(50, activation = 'relu'))
+model.add(Dropout(0.25))
+model.add(Dense(10, activation = 'relu'))
+model.add(Dense(1))
+model.compile(loss = 'mse', optimizer = 'adam')
+
+history_object = model.fit_generator(train_generator, samples_per_epoch= len(train_samples),
+validation_data=validation_generator, nb_val_samples = len(validation_samples), nb_epoch=10, verbose = 1)
+
+### print the keys contained in the history object
+print(history_object.history.keys())
+
+### plot the training and validation loss for each epoch
+plt.plot(history_object.history['loss'])
+plt.plot(history_object.history['val_loss'])
+plt.title('model mean squared error loss')
+plt.ylabel('mean squared error loss')
+plt.xlabel('epoch')
+plt.legend(['training set', 'validation set'], loc='upper right')
+plt.show()
+
+model.save('model.h5')
+
